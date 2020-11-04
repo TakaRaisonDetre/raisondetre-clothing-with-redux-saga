4958ffe8f74b6a76a93dad721002f04dc3568219
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-11-05 01:18:27 +0900

    create sshared component of custombutton and custom form input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/signin-signup/signin-signup.component';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route  path='/shop' component={ShopPage} />
+      <Route  path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     
     </div>
