4780951cf4cf49acddc33a14094ab591bbf3d8f4
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-11-08 04:41:38 +0900

    created cart-item component and connected our cart dropdown with cartItem reducer

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -7,7 +7,6 @@ const PreviewCollection = ({title, items}) =>(
     <div className='collection-preview' >
     <h1 className = 'title'>{title.toUpperCase()}</h1>
     <div className = 'preview'>
-
    {
        items
        .filter((item, idx)=> idx <4)
